3d8d3650b5353d8b82f2a089d96ab7c472e26253
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-03 08:59:08 +0300

    Modify the class-based view to list book

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,5 @@
-from django.urls import path, include
-from rest_framework import  routers
+from django.urls import path
 from .views import BookList
-
-router = routers.DefaultRouter()
-router.register(r"books", BookList, basename='book')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
