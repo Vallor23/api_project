4fc8e43d87b3d893774bb749db3aca90d2e12f85
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-03 17:09:53 +0300

    Implement views for token retrieval.

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
@@ -8,7 +9,7 @@ from .serializers import BookSerializer
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
+    permission_classes = [IsAuthenticated, IsAdminUser]
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
